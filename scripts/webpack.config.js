91f286ed45677a55dfe8ec6d7fa2a3435525ac3b
Brett Nelson <brett@WIPDeveloper.com>
2020-06-11 09:25:25 -0500

    add basic push notifications

--- scripts/webpack.config.js
+++ scripts/webpack.config.js
@@ -1,6 +1,6 @@
 // Custom webpack configuration file, provides generation of service worker
 // More information: https://developers.google.com/web/tools/workbox/modules/workbox-webpack-plugin
-const { GenerateSW } = require('workbox-webpack-plugin');
+const { InjectManifest } = require('workbox-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
@@ -10,16 +10,9 @@ module.exports = {
             './src/manifest.json',
             './src/resources'
         ]),
-        new GenerateSW({
-            swDest: 'sw.js',
-            clientsClaim: true,
-            skipWaiting: true,
-            runtimeCaching: [
-                {
-                    urlPattern: new RegExp('api/contacts$'),
-                    handler: 'StaleWhileRevalidate'
-                }
-            ]
+        new InjectManifest({
+            swSrc: './src/sw.js',
+            swDest: 'sw.js'
         })
     ]
 };
